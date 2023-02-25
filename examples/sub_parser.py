7963fd46f016481f9a5cd6c6d6eeca0fbbb278b4
elda27 <kaz.birdstick@gmail.com>
2023-02-25 23:49:57 +0900

    Update  for more practical example

--- examples/sub_parser.py
+++ examples/sub_parser.py
@@ -15,8 +15,36 @@ class SubConfigB(BaseModel):
     integer: int = Field(0, description="a required integer")
 
 
-parser = ArgumentParser()
-subparsers = parser.add_subparsers()
-build_parser(subparsers.add_parser("alpha"), SubConfigA)
-build_parser(subparsers.add_parser("beta"), SubConfigB)
-parser.print_help()
+def main():
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers()
+    alpha_parser = subparsers.add_parser("alpha")
+    build_parser(alpha_parser, SubConfigA)
+    alpha_parser.set_defaults(_command="A")
+    build_parser(subparsers.add_parser("beta"), SubConfigB)
+
+    beta_parser = subparsers.add_parser("beta")
+    build_parser(beta_parser, SubConfigB)
+    beta_parser.set_defaults(_command="B")
+    parser.set_defaults(_command="help")
+
+    args = parser.parse_args()
+
+    if args._command == "A":
+        command_a(config=SubConfigA(**vars(args)))
+    elif args._command == "B":
+        command_b(config=SubConfigA(**vars(args)))
+    else:
+        parser.print_help()
+
+
+def command_a(config: SubConfigA):
+    print(config)
+
+
+def command_b(config: SubConfigB):
+    print(config)
+
+
+if __name__ == "__main__":
+    main()
